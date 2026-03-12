341d0fcd593032b35eaca250bde94f5a67e13287
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2026-03-12 21:30:12 +0700

    Membuat kode untuk update Matakuliah(Latihan 2)

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -121,6 +121,28 @@ namespace PraktikumADO
             }
         }
 
+        private void btnUpdateMK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "UPDATE MataKuliah SET SKS = 4 WHERE KodeMK = 'IF210101'";
+                cmd = new SqlCommand(query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Jumlah data yang diupdate : " + hasil);
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
 
         public Form1()
         {
