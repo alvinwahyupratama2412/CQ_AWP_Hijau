4cb49424cab7bef0dfbbbd4c18693c903086b6f1
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2026-03-12 21:20:08 +0700

    Membuat Kode button untuk terhubung ke database

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -22,6 +22,21 @@ namespace PraktikumADO
                "Data Source=LAPTOP-5BS6SFFN\\ALVIN;Initial Catalog=DBAkademikADO;Integrated Security=True"
             );
         }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                MessageBox.Show("Koneksi ke database berhasil");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
